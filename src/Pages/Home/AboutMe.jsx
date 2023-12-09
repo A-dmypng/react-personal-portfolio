0ae8942459411c31c486b2c4f3ef36959a620454
unknown <admypung@gmail.com>
2023-12-10 00:24:26 +0800

    Update About Section

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -2,13 +2,13 @@ export default function AboutMe() {
     return (
         <section id="AboutMe" className="about--section">
             <div className="about--section--img">
-                <img src="./img/hero-image.webp" alt="About Me" />
+                <img src="./img/laptop-2.png" alt="About Me" />
             </div>
             <div className="hero--section--content--box about--section--box">
                 <div className="hero--section--content">
                     <p className="section--title">About</p>
                     <h1 className="skills--section--heading">About Me</h1>
-                    <p className="hero--section--description">Hello! I'm Ayman, a passionate and detail-oriented web developer dedicated to crafting clean, functional, and visually appealing websites. With a background in Web Design and development, I bring a unique blend of technical skills and creative thinking to every project.</p>
+                    <p className="skills--section--description">Hello! I'm Ayman, a passionate and detail-oriented web developer dedicated to crafting clean, functional, and visually appealing websites. With a background in Web Design and development, I bring a unique blend of technical skills and creative thinking to every project.</p>
                 </div>
             </div>
         </section>
