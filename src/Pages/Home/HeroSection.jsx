70712c8fa34294759f25c27c499c1158c56adc96
unknown <admypung@gmail.com>
2023-12-26 04:06:34 +0800

    Updating theme colors

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -3,7 +3,7 @@ export default function HeroSection() {
         <section id="heroSection" className="hero--section">
             <div className="hero--section--content--box">
                 <div className="hero--section--content">
-                    <p className="section--title">Hey, I'm Ayman</p>
+                    <p className="hero--sub-title">Hey, I'm Ayman</p>
                     <h1 className="hero--section--title">
                         <span className="hero--section--title--color">Web Designer</span>{" "}
                         <br />
